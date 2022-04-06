70e85684791b31cfd63ce98b12df23503897ea9d
58639436 <58639436+EnriqueLaguna@users.noreply.github.com>
2022-04-06 15:21:12 -0500

    fix input

--- randomNumerPynacl.py
+++ randomNumerPynacl.py
@@ -3,15 +3,9 @@ import math
 
 if __name__ == "__main__":
 
-    input = int(input("Numero minimo de digitos de su numero: "))
-    cantidad = math.floor((2 * math.log(input, 2)) - 2)
+    num_bytes = int(input("Numero de bytes para su numero: "))
 
-    if(cantidad < 1):
-        i = 1
-    else:
-        i = cantidad
-
-    random_byteString = nacl.utils.random(i)
+    random_byteString = nacl.utils.random(num_bytes)
     print(f"Random byteString = {random_byteString}")
     randomNumber = int.from_bytes(random_byteString, "big",signed=False)
     print(f"Numero aleatorio: {randomNumber}")
